30b2e9d2079b77e53b6144ffe5a1de93d6cec6a3
Tachera Sasi <tacherasasi@gmail.com>
2025-02-23 00:57:29 +0300

    add health check endpoint to main API

--- api/main.go
+++ api/main.go
@@ -1,1 +1,15 @@
 package main
+
+import "net/http"
+
+func main(){
+	r := http.NewServeMux()
+	r.HandleFunc("/healthcheck", healthCheck)
+
+
+}
+
+func healthCheck(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte("OK"))
+	w.WriteHeader(http.StatusOK)
+}
