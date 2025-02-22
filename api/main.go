684c0a0097bb661e5e6f82a975b760077af38da6
Tachera Sasi <tacherasasi@gmail.com>
2025-02-23 00:58:44 +0300

    add error handling for server startup in main API

--- api/main.go
+++ api/main.go
@@ -6,7 +6,10 @@ func main(){
 	r := http.NewServeMux()
 	r.HandleFunc("/healthcheck", healthCheck)
 
-
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {	
+		panic(err)
+	}
 }
 
 func healthCheck(w http.ResponseWriter,r *http.Request){
